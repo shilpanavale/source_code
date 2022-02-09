b47035ddc35a8425b2ac984df77e3cc38d183107
sachin ubale <shilpanavale49@gmail.com>
2022-02-09 09:13:20 +0530

    added new api key

--- lib/other_config.dart
+++ lib/other_config.dart
@@ -2,5 +2,5 @@
 class OtherConfig{
   static const bool USE_PUSH_NOTIFICATION = false;
   static const bool USE_GOOGLE_MAP = false;
-  static const String GOOGLE_MAP_API_KEY = "AIzaSyBrMrB6-Xt9YWUHFVqq1JHsBq3misGBKAs";
+  static const String GOOGLE_MAP_API_KEY = "AIzaSyBy5XjKkNE3oUwHoPmRMHNKtqN4ya3IlVA";
 }
