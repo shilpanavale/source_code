107e0fa6683ad5969f52d062b581397411415464
sachin ubale <shilpanavale49@gmail.com>
2022-02-24 00:01:12 +0530

    notification api

--- lib/other_config.dart
+++ lib/other_config.dart
@@ -1,6 +1,6 @@
 
 class OtherConfig{
-  static const bool USE_PUSH_NOTIFICATION = false;
+  static const bool USE_PUSH_NOTIFICATION = true;
   static const bool USE_GOOGLE_MAP = false;
   static const String GOOGLE_MAP_API_KEY = "AIzaSyBy5XjKkNE3oUwHoPmRMHNKtqN4ya3IlVA";
 }
